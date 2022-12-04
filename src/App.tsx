fdfc39b84a303fd897464fe8a0d7df845b334791
gegeeem <gegeeem@gmail.com>
2022-12-04 17:41:48 +0100

    object prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
   return (
     <div className="App">
       <Greet name="Emcane" message={10} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
