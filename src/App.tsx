faa99801a35ef5081e0dd1c2edd7dd91610e023f
gegeeem <gegeeem@gmail.com>
2022-12-07 11:28:34 +0100

    styles props React.CSSProperties

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "./components/Button";
+import { Container } from "./components/Container";
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
 import { Input } from "./components/Input";
@@ -37,6 +38,15 @@ function App() {
         value=""
         handleChange={(event) => {
           console.log(event);
+        }}
+      />
+      <Container
+        styles={{
+          border: "1px solid gray",
+          width: "80%",
+          margin: "0 auto",
+          borderRadius: "5px",
+          backgroundColor: "ButtonFace",
         }}
       />
     </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 type InputProps = {
-    value: string;
-    handleChange:(event: React.ChangeEvent<HTMLInputElement>)=>void
-}
-export const Input = (props: InputProps )=>{
-    return <input type={'text'} value={props.value} onChange={props.handleChange}/>
-}
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+export const Input = (props: InputProps) => {
+  const handleChange = (event: InputProps) => {
+    console.log(event);
+  };
+  // return (
+  //   <input type={"text"} value={props.value} onChange={handleChange} />
+  // );
+  return (
+    <input type={"text"} value={props.value} onChange={props.handleChange} />
+  );
+};
