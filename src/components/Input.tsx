51ba571443386a729b832020b65cd7c083973577
gegeeem <gegeeem@gmail.com>
2022-12-11 11:58:44 +0100

    props changes

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,23 +5,20 @@ type InputProps = {
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 // export const Input = (props: InputProps) => { before desctructuring props, and bellow after
-  //   const handleChange = (event: InputProps) => {
-  //     console.log(event);
-  //   };
-  //   // return (
-  //   //   <input type={"text"} value={props.value} onChange={handleChange} />
-  //   // );
-  //   return (
-  //     <input type={"text"} value={props.value} onChange={props.handleChange} />
-  //   );
-  // };
+//   const handleChange = (event: InputProps) => {
+//     console.log(event);
+//   };
+//   // return (
+//   //   <input type={"text"} value={props.value} onChange={handleChange} />
+//   // );
+//   return (
+//     <input type={"text"} value={props.value} onChange={props.handleChange} />
+//   );
+// };
 //after destructuring props
-   export const Input = ({value, handleChange}: InputProps) => {
-     
-     // return (
-     //   <input type={"text"} value={props.value} onChange={handleChange} />
-     // );
-     return (
-       <input type={"text"} value={value} onChange={handleChange} />
-     );
-   };
+export const Input = ({ value, handleChange }: InputProps) => {
+  // return (
+  //   <input type={"text"} value={props.value} onChange={handleChange} />
+  // );
+  return <input type={"text"} value={value} onChange={handleChange} />;
+};
